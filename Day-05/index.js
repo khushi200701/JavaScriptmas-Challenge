52ac13136df8f61998097b55a5e7987e31f8f5bf
Khushi <58480229+khushi200701@users.noreply.github.com>
2021-12-05 12:10:09 +0530

    Create index.js

---
+++ Day-05/index.js
@@ -0,0 +1,30 @@
+const greetingDisplay = document.getElementById("greeting-display")
+const btn = document.getElementById("btn")
+const bauble = document.getElementById("bauble")
+btn.addEventListener("click", writeGreeting)
+
+const greetings = [
+    "Santa Claus is coming to town!",
+    "We wish you a Merry Christmas!",
+    "Happy holidays!",
+    "Ho, ho, ho! Merry Christmas!",
+    "Jingle all the way!",
+]
+
+// Task:
+// Write a function to display a random greeting in the card.
+function writeGreeting(){
+    var fromName = window.prompt("Enter sender's name: ");
+    var toName = window.prompt("Enter reciever's name: ");
+    // console.log(fromName);
+       
+    greetingDisplay.textContent = "To Dear " + toName + ",\r\n";        
+       
+    greetingDisplay.textContent += "\r\n" + greetings[Math.floor(Math.random()*5)] + "\r\n";    
+   
+    greetingDisplay.textContent += "\r\nFrom " + fromName + "! ";
+  
+}
+// Stretch goals:
+// - Allow the user to input to and from names.
+// - Add an input for custom messages.
